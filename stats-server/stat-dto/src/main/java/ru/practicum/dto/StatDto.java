af6ad2075c09ae89434ff3f2784557793b1a4e7a
asyachem <chemerinskaya@tehnokod.com>
2025-05-19 18:43:45 +0300

    fix

--- stats-server/stat-dto/src/main/java/ru/practicum/dto/StatDto.java
+++ stats-server/stat-dto/src/main/java/ru/practicum/dto/StatDto.java
@@ -1,7 +1,6 @@
 package ru.practicum.dto;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class StatDto {
   public static void main(String[] args) {
